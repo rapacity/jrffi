14283dc10c481f779de0a98bb35b548f77d7e0a9
Khalil Harake <rapacity@codealife.com>
2011-08-24 18:46:40 +0300

    fixed jvector predicate

--- jvector.rkt
+++ jvector.rkt
@@ -9,12 +9,15 @@
      (struct _jvector jtype/vector ()
        #:property prop:procedure
        (lambda (self element)
-         (define-values (ctype make-array array-ref array-set!) (tag->array-info element))
+         (define tag (jtype-tag element))
+         (define-values (make-array array-ref array-set!) (tag->array-info tag))
          (let ([signature (make-vector-signature (jtype-signature element))])
            (struct-copy jtype/vector self
              [signature    #:parent jtype signature]
              [racket->java #:parent jtype jvector-cpointer]
              [java->racket #:parent jtype (lambda (e) (jvector e element))]
+             [predicate    #:parent jtype (λ (o) (and (jvector? o)
+                                                      (eq? tag (jtype-tag (jvector-type o)))))]
              [class        #:parent jtype/object (find-class signature)]
              [element               element]))))
      (let ([class-id (find-class "[Ljava/lang/Object;")])
