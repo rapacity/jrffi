f33ea7b3d8208b4e4224d2698b7007a40ddf69c4
Khalil Harake <rapacity@codealife.com>
2011-08-31 19:46:39 +0300

    updates to the autobinding system
    renamed jimport to jrequire, added flags for customizing the naming of bindings

--- auto.rkt
+++ auto.rkt
@@ -1,7 +1,8 @@
 #lang racket
 
 (require "core.rkt" "misc.rkt" "c.rkt" "jvector.rkt"
-         (for-syntax "query.rkt" racket/syntax syntax/parse racket/match))
+         (for-syntax "query.rkt" racket/syntax syntax/parse racket/match srfi/26/cut racket/list
+           racket/string))
 
 (begin-for-syntax
   (define default-autobind-typemap
@@ -34,18 +35,85 @@
         [(list type) (ref type)]))
     map-type)
   
-  (define (construct-syntax class-name stx token->type)
-    (define class-name-forward-slash (regexp-replace* #rx"[.]" class-name "/"))
-    (define class-info (find-class-signature class-name))
+  (define (import-renamer name)
+    (last (regexp-split #rx"[.]" name)))
+  
+  (define (split-dots str)
+    (regexp-split #rx"[.]" str))
+  
+  (define (split-camel-case str)
+    (regexp-split #rx"(?<=[a-z])(?=[A-Z])" str))
+  
+  (define (hyphenate lst)
+    (string-join lst "-"))
+  
+  (define (translate-words str)
+    (match str
+      ["to" "->"]
+      ["To" "->"]
+      [else str]))
+  
+  (define (fix-hyphens str)
+    ((compose (cut regexp-replace* #rx"-+" <> "-")
+              (cut regexp-replace* #rx">-+" <> ">")
+              (cut regexp-replace* #rx"_+" <> "-"))
+     str))
+    
+  (define (racketify str)
+    (string-downcase(hyphenate (map translate-words (append* (map split-camel-case (split-dots str)))))))
+  
+  (define (racketify-namer type class-name field-or-method-name)
+    (with-handlers ([void (λ (e) (error (format "error: racketify: ~a ~a ~a~%"
+                                                type class-name field-or-method-name)))])
+      (case type
+        [(class)       (fix-hyphens (racketify class-name))]
+        [(predicate)   (fix-hyphens (format "~a?" (racketify class-name)))]
+        [(mutator)     (fix-hyphens (format "set-~a-~a!"
+                                            (racketify class-name)
+                                            (racketify field-or-method-name)))]
+        [(accessor)    (fix-hyphens (format "get-~a-~a"
+                                            (racketify class-name)
+                                            (racketify field-or-method-name)))]
+        [(constructor) (fix-hyphens (format "make-~a" (racketify class-name)))]
+        [(method)      (fix-hyphens (format "~a-~a"
+                                            (racketify class-name)
+                                            (racketify field-or-method-name)))])))
+  
+  (define (default-namer type class-name field-or-method-name)
+    (case type
+      [(class)       class-name]
+      [(predicate)   (format "~a?" class-name)]
+      [(mutator)     (format "set-~a-~a!" class-name field-or-method-name)]
+      [(accessor)    (format "get-~a-~a" class-name field-or-method-name)]
+      [(constructor) (format "new-~a" class-name)]
+      [(method)      (format "~a-~a" class-name field-or-method-name)]))
+  
+  
+  (define (construct-syntax stx token->type import? racketify? custom-namer)
+    (define namer (or (and racketify? racketify-namer)
+                        custom-namer
+                        default-namer
+                        ))
+    (define (namer-stx type class-name field-or-method-name)
+      (format-id stx (namer type class-name field-or-method-name)))
+    (define package+class-name (symbol->string (syntax-e stx)))
+    (define package+class-name-forward-slash (regexp-replace* #rx"[.]" package+class-name "/"))
+    (define class-info (find-class-signature package+class-name))
     (define class-methods
       (partition-by method-signature-name (jclass-signature-methods class-info)))
     (define class-constructors (jclass-signature-constructors class-info))
     (define class-fields       (jclass-signature-fields class-info))
-    (define class-identifier (format-id stx "~a" class-name))
+    (define class-name         (if import? (import-renamer package+class-name) package+class-name))
+    (define class-identifier   (namer-stx 'class class-name #f))
     #`(begin
-        (define #,class-identifier (#,(format-id stx "find-class") #,class-name-forward-slash))
+        (define #,class-identifier
+          (#,(format-id stx "find-class") #,package+class-name-forward-slash ))
+        
+        (define (#,(namer-stx 'predicate class-name #f) obj)
+          (instance-of? obj #,class-identifier))
+        
         #,@(if (null? class-constructors) #`()
-               #`((define #,(format-id stx "new-~a" class-name)
+               #`((define #,(namer-stx 'constructor class-name #f)
                     (jconstructor/overload/check #,class-identifier 
                      #,@(map (match-lambda [(constructor-signature vararg? args return)
                                             (list vararg? (map token->type args)
@@ -53,7 +121,7 @@
         #,@(for/list ([i (in-list class-methods)])
              (match i
                [(list-rest method-name methods)
-                #`(define #,(format-id stx "~a-~a" class-name method-name)
+                #`(define #,(namer-stx 'method class-name method-name)
                     (jmethod/overload/check
                      #,class-identifier #,method-name 
                      #,@(map (match-lambda [(method-signature _ _ static? _ vararg? args return)
@@ -62,14 +130,14 @@
         #,@(for/list ([i (in-list class-fields)])
              (match i
                [(field-signature field-name final? static? type)
-                #`(define #,(format-id stx "get-~a-~a" class-name field-name)
+                #`(define #,(namer-stx 'accessor class-name field-name)
                     (get-jaccessor #,class-identifier #,field-name #,(token->type type)
                                    #:static? #,static?))]))
         #,@(for/fold ([output null]) ([i (in-list class-fields)])
              (match i
                [(field-signature field-name final? static? type)
                 (if final? output
-                    (cons #`(define #,(format-id stx "~set-~a-~a!" class-name field-name)
+                    (cons #`(define #,(namer-stx 'mutator class-name field-name)
                               (get-jmutator #,class-identifier #,field-name #,(token->type type)
                                             #:static? #,static?))
                           output))])))))
@@ -77,11 +145,50 @@
   
 
 
-(define-syntax (jimport stx)
+(define-syntax (jrequire stx)
   (syntax-parse stx
-    [(_ class-name:id)
-     (let ([token->type (type-mapper default-autobind-typemap)])
-       (construct-syntax (symbol->string (syntax-e #'class-name)) #'class-name token->type))]))
+    [(_ class-name:id ...
+        (~optional (~and #:import import-kw))
+        (~optional (~or (~and #:racketify racketify-kw)
+                        (~seq #:renamer renamer-proc)))
+        (~optional (~seq #:typemap typemap-proc)))
+     (let* ([token->type (type-mapper default-autobind-typemap)]
+            [unpacker (cut construct-syntax <> token->type
+                           (attribute import-kw) 
+                           (attribute racketify-kw)
+                           (and (attribute renamer-proc) (eval-syntax #'renamer-proc))
+                           )])
+       (with-syntax ([(unpacked ...) (map unpacker (syntax-e #`(class-name ...)))])
+         #`(begin unpacked ...)))]))
 
 
 (provide (all-defined-out))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- tests/auto.rkt
+++ tests/auto.rkt
@@ -1,20 +1,37 @@
 #lang racket
 
-(require rackunit "../main.rkt" "../auto.rkt")
+(require rackunit (except-in "../main.rkt" Integer) "../auto.rkt")
 
 
-(jimport java.util.Arrays)
+(jrequire java.util.Arrays java.lang.Integer)
 
 
 (define x (make-jvector _jboolean 2))
 (jvector-set! x 0 #f)
 (jvector-set! x 1 #t)
 
-(test-equal? "jimport method" "[false, true]" (java.util.Arrays-toString x))
+(test-equal? "jrequire method" "[false, true]" (java.util.Arrays-toString x))
 
-(jimport java.lang.Integer)
+(test-equal? "jrequire constructor/method" "444" (java.lang.Integer-toString (new-java.lang.Integer "444")))
+
+(test-equal? "jrequire field-accessor" 2147483647 (get-java.lang.Integer-MAX_VALUE))
+
+(jrequire java.lang.Integer #:import)
+
+(test-equal? "jrequire constructor/method #:import" "444" (Integer-toString (new-Integer "444")))
+(test-equal? "jrequire field-accessor #:import" 2147483647 (get-Integer-MAX_VALUE))
+
+(jrequire java.lang.Integer #:import #:racketify)
 
 
-(test-equal? "jimport constructor/method" "444" (java.lang.Integer-toString (new-java.lang.Integer "444")))
+(test-equal? "jrequire constructor/method #:import #:racketify" "100"
+             (integer->string (make-integer 100)))
+(test-equal? "jrequire field-accessor #:import #:racketify" 2147483647
+             (get-integer-max-value))
+(test-true "jrequire predicate? #:import #:racketify"
+           (integer? (make-integer 100)))
 
-(test-equal? "jimport field-accessor" 2147483647 (get-java.lang.Integer-MAX_VALUE))
+(require ffi/unsafe)
+
+(test-true "jrequire class #:import #:racketify"
+           (cpointer-has-tag? integer 'jclass))
